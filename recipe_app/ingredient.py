97c749a5a5800b53a2ee2515921589ed41b68b06
Ордынская Мираслава <x8honor@gmail.com>
2026-06-02 21:41:25 +0300

    Реализован класс Ingredient

--- recipe_app/ingredient.py
+++ recipe_app/ingredient.py
@@ -1,1 +1,25 @@
-
+class Ingredient:
+    def __init__(self, name: str, quantity: float, unit: str):
+        self.name = name
+        self.unit = unit
+        self.quantity = quantity
+        
+    @property
+    def quantity(self) -> float:
+        return self._quantity
+    
+    @quantity.setter
+    def quantity(self, value):
+        float_value = float(value)
+        if float_value <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self._quantity = float_value
+        
+    def __str__(self) -> str:
+        return f"{self.name}: {self.quantity} {self.unit}"
+    
+    def __repr__(self) -> str:
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+    
+    def __eq__(self, other: object) -> bool:
+        return self.name == other.name and self.unit == other.unit
