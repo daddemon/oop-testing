1d183deb639dead38180a0267a494501d0a408a2
Ордынская Мираслава <x8honor@gmail.com>
2026-06-03 01:18:17 +0300

    Исправлены отступы

--- recipe_app/recipe.py
+++ recipe_app/recipe.py
@@ -18,11 +18,10 @@ class Recipe:
         return isinstance(ratio, (int, float)) and ratio > 0
     
     def scale(self, ratio: float):
-      scaled_ing = []
-      for ing in self.ingredients:
+        scaled_ing = []
+        for ing in self.ingredients:
           new_ing = Ingredient(name=ing.name, quantity=ing.quantity * ratio, unit=ing.unit)
           scaled_ing.append(new_ing)
-        
         return Recipe(title=self.title, ingredients=scaled_ing)
     
     def __len__(self):
